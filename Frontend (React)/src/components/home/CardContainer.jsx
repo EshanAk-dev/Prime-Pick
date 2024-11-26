0ee99d42cb1dd41258dbe39eb14ff99366edf328
EshanAkalanka <163629582+EshanAk44@users.noreply.github.com>
2024-11-26 15:40:24 +0530

    Update CardContainer.jsx

--- Frontend (React)/src/components/home/CardContainer.jsx
+++ Frontend (React)/src/components/home/CardContainer.jsx
@@ -1,17 +1,30 @@
-import HomeCard from "./HomeCard"
+import HomeCard from "./HomeCard";
 
-const CardContainer = ({products}) => {
+const CardContainer = ({ products }) => {
   return (
     <section className="py-5" id="shop">
-        <h4 style={{ textAlign: "center" }}>Our Products</h4>
+      <h4
+        style={{
+          textAlign: "center",
+          fontSize: "2rem",
+          fontWeight: "bold",
+          textShadow: "2px 2px 5px rgba(0, 0, 0, 0.4)",
+          marginBottom: "20px",
+          textTransform: "uppercase"
+        }}
+      >
+        New Products
+      </h4>
 
-        <div className="container px-4 px-lg-5 mt-5">
-            <div className="row justify-content-center">
-              {products.map(product => <HomeCard key={product.id} product={product} />)}
-            </div>
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row justify-content-center">
+          {products.map((product) => (
+            <HomeCard key={product.id} product={product} />
+          ))}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default CardContainer
+export default CardContainer;
