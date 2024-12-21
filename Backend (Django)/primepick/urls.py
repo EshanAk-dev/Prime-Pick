e47d29d3052d87d033df0c4be86e006192c9ba85
EshanAkalanka <163629582+EshanAk44@users.noreply.github.com>
2024-12-21 13:39:46 +0530

    Update urls.py

--- Backend (Django)/primepick/urls.py
+++ Backend (Django)/primepick/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("shop_app.urls"))
+    path("", include("shop_app.urls")),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
